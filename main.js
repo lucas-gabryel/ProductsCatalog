812da4daf98fb43f33528a58017e720a89267dc2
Lucas <lucas-gabryel@outlook.com>
2024-09-08 00:40:12 -0300

    creating the search script

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-const sectionProdutos = document.getElementById('produtos')
-/* console.log(sectionProdutos) */
+let resultados = '';
+let sectionProdutos = document.getElementById('produtos');
+
+function pesquisar() {
+    resultados = ''
+    let campoPesquisa = document.getElementById('input-pesquisa').value;
+    
+    if(!campoPesquisa){
+        sectionProdutos.innerHTML = `<p>Nenhum produto encontrado. Digite algum produto, tamanho ou cor.</p>`
+    };
+    
+    campoPesquisa = campoPesquisa.toLowerCase();
+    
+    
+    let title = '';
+    let size = '';
+    let color = '';
+    
+    for(let produto of dataProdutos){
+        title = produto.title.toLowerCase();
+        size = produto.size.toLowerCase();
+        color = produto.color.toLowerCase();
+    
+        if(title.includes(campoPesquisa) || size.includes(campoPesquisa) || color.includes(campoPesquisa)) {
+            resultados += `
+                <div class="produto">
+                    <div>
+                        <img id="img" src="${produto.img}">
+                    </div>
+                    <h2 id="titulo">${produto.title}</h2>
+                    <p><span>R$ <span id="preco">${produto.price}</span></span> à vista</p>
+                </div>`;
+        };
+    
+    };
+    
+    if(!resultados){
+        resultados = `<p>Nenhum produto encontrado.</p>`
+    };
+    
+    sectionProdutos.innerHTML = resultados;
+}
 
 for(let produto of dataProdutos){
-    sectionProdutos.innerHTML += `
+    resultados += `
         <div class="produto">
             <div>
                 <img id="img" src="${produto.img}">
             </div>
             <h2 id="titulo">${produto.title}</h2>
             <p><span>R$ <span id="preco">${produto.price}</span></span> à vista</p>
-        </div>`
-}
+        </div>`;
+    };
+
+sectionProdutos.innerHTML = resultados;
